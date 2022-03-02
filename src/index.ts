452497bab546f5291c1a65f2ced587204c1774fb
owencalvin <owen.gombas@gmail.com>
2022-03-02 12:28:19 +0100

    binary substraction

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { BinaryHelper } from "./BinaryHelper";
 const bh = new BinaryHelper(8);
 
 const resAdd = bh.binaryAddition("00111111", "11100011");
-const resSub = bh.binarySubstraction("101010000", "11111101");
+const resSub = bh.binarySubstraction("10101000", "11111101");
 
+console.log(resAdd);
 console.log(resSub)
