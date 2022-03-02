29c04485556a543c1f40ea797a2f64af9f51820e
owencalvin <owen.gombas@gmail.com>
2022-03-02 12:25:20 +0100

    binary substraction

--- src/BinaryHelper.ts
+++ src/BinaryHelper.ts
@@ -5,14 +5,24 @@ export class BinaryHelper {
     this._n = n;
   }
 
-  public getNBit(value: 1 | 0) {
+  public getNBit(value: 1 | 0, n = this._n): string {
     let res = "";
     
-    for (let i = 0; i < this._n; i++) {
+    for (let i = 0; i < n; i++) {
       res += value;
     }
 
-    return Number(res);
+    return res;
+  }
+
+  public addPadding(b: string) {
+    const size = this._n - b.length;
+
+    for (let i = 0; i < size; i++) {
+      b = "0" + b;
+    }
+
+    return b;
   }
 
   /**
@@ -21,7 +31,7 @@ export class BinaryHelper {
    * @returns The binary representation of the decimal number
    */
   public decimalToBinary(decimal: number) {
-    // return (decimal >>> 0).toString(2)
+    return (decimal >>> 0).toString(2)
   }
 
   /**
@@ -30,7 +40,7 @@ export class BinaryHelper {
    * @returns The int representation of a binary number
    */
   public binaryToDecimal(binary: string) {
-    // return parseInt(binary, 2);
+    return parseInt(binary, 2);
   }
 
   public elementaryAddition(x: string, y: string, carry = "0"): string[] {
@@ -61,5 +71,25 @@ export class BinaryHelper {
     }
 
     return [res, carry];
+  }
+
+  public invert(b: string) {
+    b = this.decimalToBinary(this.binaryToDecimal(b) ^ this.binaryToDecimal(this.getNBit(1)));
+    b = this.addPadding(b);
+    return b;
+  }
+
+  public addOne(b: string) {
+    return this.binaryAddition(b, this.getNBit(0, 7) + "1")[0];
+  }
+
+  public binarySubstraction(b1: string, b2: string) {
+    b2 = this.invert(b2);
+    b2 = this.addOne(b2);
+
+    const res = this.binaryAddition(b1, b2);
+    res[0] = this.invert(res[0]);
+    res[0] = this.addOne(res[0]);
+    return res;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { BinaryHelper } from "./BinaryHelper";
 
 const bh = new BinaryHelper(8);
 
-const res = bh.binaryAddition("00111111", "11100011");
+const resAdd = bh.binaryAddition("00111111", "11100011");
+const resSub = bh.binarySubstraction("101010000", "11111101");
 
-console.log(res)
+console.log(resSub)
