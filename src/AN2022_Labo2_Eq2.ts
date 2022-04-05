e507d931b381bc86eec94effdfacdd86384cc8f7
owen <owen@venmac.local>
2022-04-05 15:31:28 +0200

    labo2

--- src/AN2022_Labo2_Eq2.ts
+++ src/AN2022_Labo2_Eq2.ts
@@ -11,26 +11,90 @@ import { Dichotomy } from "./classes/Dichotomy";
 const erEquationElement = <HTMLInputElement>document.getElementById("er-equation");
 const erAElement = <HTMLInputElement>document.getElementById("er-a");
 const erBElement = <HTMLInputElement>document.getElementById("er-b");
+const erStepElement = <HTMLInputElement>document.getElementById("er-step");
 const erResultElement = document.getElementById("er-result");
+const erPlotElement = document.getElementById("er-plot");
+
+function removeGraph() {
+  erPlotElement.style.display = 'none';
+}
+
+function displayGraph(a: number, b: number, roots: number[]) {
+  erPlotElement.style.display = 'block';
+
+  const annotations = roots.map((x, index) => ({
+    x,
+    text: `x${index} ≈ ${x}`
+  }));
+
+  const parameters = {
+    target: '#er-plot',
+    data: [{
+      fn: erEquationElement.value.replace(/Math\./gi, "").replace(/\*\*/gi, "^")
+    }],
+    width: 800,
+    height: 500,
+    grid: false,
+    yAxis: { domain: [-1, 1] },
+    xAxis: { domain: [a, b] },
+    annotations
+  };
+
+  // @ts-ignore
+  functionPlot(parameters);
+}
 
 function onChangeEquation() {
+  let result = "";
+
   if (
     erAElement.value === "" ||
-    erBElement.value === ""
+    erBElement.value === "" ||
+    erStepElement.value === ""
   ) {
     erResultElement.innerHTML = `<span class="color-grey">Veuillez renseigner tous les champs</span>`;
+    removeGraph();
+    return;
+  }
+
+  const erA = Number(erAElement.value);
+  const erB = Number(erBElement.value);
+  const erStep = Number(erStepElement.value);
+
+  if (Number.isNaN(erA) || Number.isNaN(erB) || Number.isNaN(erStep)) {
+    erResultElement.innerHTML = `<span class="color-grey">Les valeurs d'intervalle et de step doivent être des nombres</span>`;
+    removeGraph();
+    return;
+  }
+
+  if (erA >= erB) {
+    erResultElement.innerHTML = `<span class="color-red">La première valeur de l'intervalle doit être plus grande que la deuxième</span>`;
+    removeGraph();
     return;
   }
 
   const erEquation = (x: number) => eval(erEquationElement.value);
-  const erA = Number(erAElement.value);
-  const erB = Number(erBElement.value);
+  let roots: number[];
 
-  const result = Dichotomy.calculate(erA, erB, (x: number) => Math.sin(x) - x / 13);
+  try {
+    roots = Dichotomy.calculateAll(erA, erB, erStep, erEquation);
+    result = roots
+        .map(
+            (x, index) => `<p class="mono">x<sub class="mono">${index}</sub> ≈ ${x}</p>`
+        )
+        .join("");
+  } catch (err) {
+    console.log(err);
+    erResultElement.innerHTML = `<span class="color-red">Votre équation est invalide</span>`;
+    removeGraph();
+    return;
+  }
+
+  displayGraph(erA, erB, roots);
 
   erResultElement.innerHTML = `
-    <div class="result-group color-grey">
-      Résultat ${result}
+    <div class="result-group">
+      Résultats: ${result}
     </div>
   `;
 }
@@ -39,5 +103,7 @@ erAElement.addEventListener("change", onChangeEquation);
 erBElement.addEventListener("keyup", onChangeEquation);
 erEquationElement.addEventListener("change", onChangeEquation);
 erEquationElement.addEventListener("keyup", onChangeEquation);
+erStepElement.addEventListener("change", onChangeEquation);
+erStepElement.addEventListener("keyup", onChangeEquation);
 
 onChangeEquation();
--- src/classes/Dichotomy.ts
+++ src/classes/Dichotomy.ts
@@ -1,13 +1,5 @@
 export class Dichotomy {
     static calculate(a: number, b: number, f: (x: number) => number) {
-        if (a > b) {
-            return null;
-        }
-
-        if (a === b) {
-            return null;
-        }
-
         let fA = f(a);
         let mNew = a + b;
         let mOld = 2 * mNew;
@@ -26,5 +18,116 @@ export class Dichotomy {
         }
 
         return mNew;
+    }
+
+    static calculateIntervals(a: number, b: number, step: number, f: (x: number) => number): number[][] {
+        /**
+         * intervalsCoordinates gives a list of coordinates with a f(x) that is alternatively positive and negatives
+         * so it means that between each x values there is a root
+         * the while just under calculate for each x separated by a "little step" it's f(x) value.
+         * And if f(x) has a different sign than f(x) of the previous x, then we add the coordinates in the list.
+         *
+         * intervalsCoordinates = [
+         *      [  x                    |  f(x)                 ]
+         *      -------------------------------------------------
+         *      [  -100,                   8.198673333417451    ],
+         *      [  -8.649999999999078,    -0.034172813218782294 ],
+         *      [  -6.699999999999074,     0.11053469476879291  ],
+         *      [  -2.7999999999990686,   -0.11960353477223878  ],
+         *      [  0.05000000000093055,    0.04613301542538229  ],
+         *      [  3.0500000000009297,    -0.14315074238394498  ],
+         *      [  6.950000000000936,      0.08387074354830293  ],
+         *      [  8.75000000000094,      -0.04835296932353672  ]
+         * ]
+         */
+        const intervalsCoordinates = [
+            [a, f(a)]
+        ];
+        let j = a + step;
+
+        while (j < b) {
+            const [_, fi] = intervalsCoordinates[intervalsCoordinates.length - 1];
+            const fj = f(j);
+
+            if (fi * fj < 0) {
+                intervalsCoordinates.push([
+                    j,
+                    fj
+                ]);
+            }
+
+            j += step;
+        }
+
+        /**
+         * intervals merge the intervalsCoordinates's x values to create a list
+         * of intervals that contains a root value, so now we juste have to call the method calculate(a, b, f)
+         * to determine all the root values
+         *
+         * intervalsCoordinates = [                                       |         intervals = [
+         *      [  x                    |  f(x)                      ]    |         [ a                  | b                   ]
+         *      ------------------------------------------------------    |         --------------------------------------------
+         *      [  ---> -100,                  8.198673333417451     ],   |
+         *                                                                | -------> [ -100,                -8.649999999999078  ]
+         *      [  ---> -8.649999999999078,    -0.034172813218782294 ],   |
+         *                                                                | -------> [ -8.649999999999078,  -6.699999999999074  ]
+         *      [  ----> -6.699999999999074,   0.11053469476879291   ],   |
+         *                                                                | -------> [ -6.699999999999074,  -2.7999999999990686 ]
+         *      [  ----> -2.7999999999990686,  -0.11960353477223878  ],   |
+         *                                                                | -------> [ -2.7999999999990686, 0.05000000000093055 ]
+         *      [  ----> 0.05000000000093055,  0.04613301542538229   ],   |
+         *                                                                | -------> [ 0.05000000000093055, 3.0500000000009297  ]
+         *      [  ----> 3.0500000000009297,   -0.14315074238394498  ],   |
+         *                                                                | -------> [ 3.0500000000009297,  6.950000000000936   ]
+         *      [  ----> 6.950000000000936,    0.08387074354830293   ],   |
+         *                                                                | -------> [ 6.950000000000936,   8.75000000000094    ]
+         *      [  ----> 8.75000000000094,     -0.04835296932353672  ]    |          ]
+         * ]
+         *
+         * So intervals gives:
+         * intervals = [
+         *      [  a                     |   b                    ]
+         *      ---------------------------------------------------
+         *      [  -100,                     -8.649999999999078   ],
+         *      [  -8.649999999999078,       -6.699999999999074   ],
+         *      [  -6.699999999999074,       -2.7999999999990686  ],
+         *      [  -2.7999999999990686,      0.05000000000093055  ],
+         *      [  0.05000000000093055,      3.0500000000009297   ],
+         *      [  3.0500000000009297,       6.950000000000936    ],
+         *      [  6.950000000000936,        8.75000000000094     ]
+         * ]
+         */
+        const intervals = intervalsCoordinates.reduce((prev, value) => {
+            const last = prev[prev.length - 1];
+            if (last.length >= 2) {
+                prev.push([last[1], value[0]]);
+                prev.push([value[0]]);
+            } else {
+                last.push(value[0]);
+            }
+            return prev;
+        }, [[]]);
+
+        return intervals;
+    }
+
+    static calculateAll(a: number, b: number, step: number, f: (x: number) => number) {
+        const roots = [];
+        const intervals = this.calculateIntervals(a, b, step, f);
+
+        for (const [a, b] of intervals) {
+            const x = this.calculate(a, b, f);
+            const fx = this.round(f(x));
+
+            if (fx === 0) {
+                roots.push(this.round(x));
+            }
+        }
+
+        return roots;
+    }
+
+    static round(x: number) {
+        return Math.round((x + Number.EPSILON) * 10000000) / 10000000;
     }
 }
